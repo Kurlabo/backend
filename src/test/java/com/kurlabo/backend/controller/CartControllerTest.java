5703a1f8201f4757494c17f947f3ef868dfd9dcd
LNoah <limnoah0301@gmail.com>
2021-02-06 01:21:46 +0900

    feat: Add main page / cart page API
    Add main page API, cart page API, and both of Test class.

---
+++ src/test/java/com/kurlabo/backend/controller/CartControllerTest.java
@@ -0,0 +1,45 @@
+package com.kurlabo.backend.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+class CartControllerTest {
+
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void before(WebApplicationContext wac) {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac)
+                .addFilter(new CharacterEncodingFilter("UTF-8", true))
+                .alwaysDo(print())
+                .build();
+    }
+
+    @DisplayName("CartTest")
+    @Test
+    void cartTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/shop/goods/goods_cart"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("productInfoDto").value(""))
+                .andExpect(jsonPath("userInfoDto").value(""));
+
+    }
+
+}

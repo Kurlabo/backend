ec1fc149073950c9cdb7c50ea5191cb98397c38c
yus2on <gbwl2784@gmail.com>
2021-02-08 04:05:28 +0900

    feat: Add board API
    - Add BoardDto
    - Add BoardController
    - Add BoardControllerTest

---
+++ src/main/java/com/kurlabo/backend/dto/testdto/BoardDto.java
@@ -0,0 +1,26 @@
+package com.kurlabo.backend.dto.testdto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BoardDto {
+    private Long boardId;
+
+    private String title;
+
+    private String writer;
+
+    private LocalDateTime regdate;
+
+    private Integer cnt;
+
+    private String content;
+}

6a1555f285fcfe7487f004ac6ee75d3a9bae697b
theVebper <immortaldk@gmail.com>
2021-02-15 15:36:08 +0900

    fix : for merge purposes.

---
+++ src/main/java/com/kurlabo/backend/model/Member.java
@@ -0,0 +1,36 @@
+package com.kurlabo.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="member_id")
+    private Long id;
+
+    private String uid;
+
+    private String password;
+
+    private String name;
+
+    private String email;
+
+    private String phone;
+
+    private String gender;
+
+    private String date_of_birth;
+
+    private String grade;
+
+    private int total_cost;
+}

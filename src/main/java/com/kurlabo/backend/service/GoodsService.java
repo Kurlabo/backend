187a80779248c8264d31dd3d532430a11fe2c779
yus2on <gbwl2784@gmail.com>
2021-02-18 05:22:55 +0900

    feat: Add GoodsService and GoodsServiceTest

--- src/main/java/com/kurlabo/backend/service/GoodsService.java
+++ src/main/java/com/kurlabo/backend/service/GoodsService.java
@@ -1,8 +1,12 @@
 package com.kurlabo.backend.service;
 
+import com.kurlabo.backend.dto.ProductDto;
+import com.kurlabo.backend.exception.ResourceNotFoundException;
 import com.kurlabo.backend.model.Cart;
 import com.kurlabo.backend.model.Member;
+import com.kurlabo.backend.model.Product;
 import com.kurlabo.backend.repository.CartRepository;
+import com.kurlabo.backend.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class GoodsService {
 
     private final CartRepository cartRepository;
+    private final ProductRepository productRepository;
 
     @Transactional
     public void insertCart(Member member, Long product_id, int cnt){
@@ -21,4 +26,14 @@ public class GoodsService {
         }
         cartRepository.save(new Cart(null, product_id, cnt, member));
     }
+
+
+    @Transactional
+    public ProductDto goodDetail(Long id) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(ResourceNotFoundException::new);
+
+        return new ProductDto(product);
+    }
+
 }

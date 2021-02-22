351b24f34a72cf91d45503959c8dd544f605af55
LNoah <limnoah0301@gmail.com>
2021-02-22 15:34:22 +0900

    feat: Add getOrderView method in OrderService.
    Added getOrderView method.

--- src/main/java/com/kurlabo/backend/service/OrderService.java
+++ src/main/java/com/kurlabo/backend/service/OrderService.java
@@ -1,20 +1,23 @@
 package com.kurlabo.backend.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.kurlabo.backend.converter.StringRevisor;
-import com.kurlabo.backend.dto.order.CheckoutRequestDto;
-import com.kurlabo.backend.dto.order.OrderSheetRequestDto;
-import com.kurlabo.backend.dto.order.OrderSheetResponseDto;
+import com.kurlabo.backend.dto.order.*;
 import com.kurlabo.backend.exception.ResourceNotFoundException;
 import com.kurlabo.backend.model.Deliver_Address;
 import com.kurlabo.backend.model.Member;
 import com.kurlabo.backend.model.Orders;
-import com.kurlabo.backend.repository.*;
+import com.kurlabo.backend.model.Product;
+import com.kurlabo.backend.repository.DeliverAddressRepository;
+import com.kurlabo.backend.repository.MemberRepository;
+import com.kurlabo.backend.repository.OrderRepository;
+import com.kurlabo.backend.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -24,6 +27,7 @@ public class OrderService {
     private final MemberRepository memberRepository;
     private final DeliverAddressRepository deliverAddressRepository;
     private final OrderRepository orderRepository;
+    private final ProductRepository productRepository;
     private final ObjectMapper objectMapper;
 
     public OrderSheetResponseDto getOrderSheet(Member member){
@@ -45,17 +49,19 @@ public class OrderService {
     }
 
     @Transactional
-    public String setCheckout(CheckoutRequestDto dto) {
-        Member mem = memberRepository.findById(dto.getMember_id()).orElseThrow(
-                () -> new ResourceNotFoundException()
-        );
-        String productIdCntListStr = "";
+    public String setCheckout(CheckoutRequestDto dto) throws JsonProcessingException {
+        List<OrderProductDto> orderProductDtos = new ArrayList<>();
+        Member mem = memberRepository.findById(dto.getMember_id()).orElseThrow(ResourceNotFoundException::new);
         String returnStr = "결제에 성공하셨습니다.";
 
-        try{
-            productIdCntListStr = objectMapper.writeValueAsString((dto.getProduct_id_list()));
-        } catch (Exception e){
-            e.printStackTrace();
+        for(OrderListDto list: dto.getProduct_id_list()){
+            Product product = productRepository.findById(list.getProduct_id()).orElseThrow(ResourceNotFoundException::new);
+            orderProductDtos.add(new OrderProductDto(
+                    list.getProduct_id(),
+                    product.getName(),
+                    product.getDiscounted_price() * list.getCnt(),
+                    list.getCnt()
+            ));
         }
 
         Orders orders = new Orders(
@@ -71,7 +77,7 @@ public class OrderService {
                 dto.getCheckout(),
                 "배송중",
                 dto.getArrived_alarm(),
-                productIdCntListStr,
+                objectMapper.writeValueAsString(orderProductDtos),
                 dto.getTotal_cost(),
                 mem
         );
@@ -79,5 +85,30 @@ public class OrderService {
         orderRepository.save(orders);
 
         return returnStr;
+    }
+
+    public OrderViewResponseDto getOrderView(Long order_id) throws JsonProcessingException {
+        Orders order = orderRepository.findById(order_id).orElseThrow(ResourceNotFoundException::new);
+
+        List<OrderProductDto> orderProducts = objectMapper.readValue(
+                order.getProduct_id_cnt_list(),
+                new TypeReference<List<OrderProductDto>>() {}
+        );
+
+        return new OrderViewResponseDto(
+                orderProducts,
+                order.getDelivery_condition(),
+                order.getTotal_cost(),
+                order.getCheckout(),
+                order.getOrderer(),
+                order.getSender(),
+                order.getCheckout_date(),
+                order.getReciever(),
+                order.getReciever_phone(),
+                order.getReciever_post(),
+                order.getReciever_place(),
+                order.getReciever_visit_method(),
+                order.getArrived_alarm()
+        );
     }
 }

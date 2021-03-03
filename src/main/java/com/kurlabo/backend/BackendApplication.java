48ed9558fdb70a9a7e45a87b88b28ffc14abaf07
LNoah <limnoah0301@gmail.com>
2021-03-04 03:00:45 +0900

    refactor: refactor mainApplication Class.

--- src/main/java/com/kurlabo/backend/BackendApplication.java
+++ src/main/java/com/kurlabo/backend/BackendApplication.java
@@ -14,9 +14,4 @@ public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
 }

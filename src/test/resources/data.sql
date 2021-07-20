31a755bda92bb86fed27e94c96c02b9ed04f3e61
LNoah <limnoah0301@gmail.com>
2021-07-21 00:06:58 +0900

    conf: Add data.sql, Update application.yml

---
+++ src/test/resources/data.sql
@@ -0,0 +1,307 @@
+-- board
+insert into board (board_id, cnt, content, regdate, title, writer) values(
+                            null,
+                            7093,
+                            '주문 취소는 제품 배송 전일 오후 6시까지 고객 행복 센터(1644-1107) / 1:1 문의 게시판 / 카카오톡(마켓컬리)로 접수 부탁드립니다.\n
+                            - 오후 6시 이후에는 주문 취소가 불가합니다. (예약딜 포함)\n
+                            - 고객 행복 센터(1644-1107) 운영 종료 시간인 오후 4시 이후부터는 1:1문의 게시판 접수만 가능합니다.\n
+                            - 예약 상품은 배송 3~4일 전에만 주문 취소가 가능합니다.\n
+                            - 정확한 처리를 위해 주문번호는 필수로 입력해주세요.\n\n
+                            * 비회원 주문건의 경우 1:1 접수가 불가하기에, 취소 및 주분변경 관련하여\n
+                            배송 전일 오후 4시까지 고객 행복 센터(1644-1107)/카카오톡(마켓컬리)로 접수해주시면 확인하여 조치해드립니다.',
+                            '2015-05-21',
+                            '취소/반품/환불요청은 어떻게 하나요?',
+                            'MarketKurly'
+                        );
+insert into board (board_id, cnt, content, regdate, title, writer) values(
+                            null,
+                            6064,
+                            '1/12일 전국 배송이 시작하게 됨에 따라 사용하고 있던 시스템을 이전(카페24->고도몰)하게 되었습니다.\n\n
+                            기존 시스템을 그대로 이용하면서 일정 부분을 변경하는 것이라면, 비번의 변경이 필요 없겠지만, 기존에 사용하던 시스템에서 완전히 체계가 다른 시스템으로 사이트가 이전을 하다보니, 기존 사이트에서의 정보를 이전하는 작업이 필요하였습니다.\n\n
+                            정보보호를 위해 고객님의 비번은 저장되지 않으며, 관리자라 할지라고 정보를 조회하거나 트래킹할 수 없기에 신규사이트로 정보를 이전을 하면서 부득이하게 임시번호를 부여해 드렸음을 알려드립니다.\n\n
+                            (관련하여 1/11~1/12일 기간에 회원가입 시 등록해 주신 휴대폰으로 관련 정보에 대해 SMS 발송 드렸습니다.)\n\n
+                            SMS를 수신하기 못하셨거나 추가적으로 궁금한 점이 있으실 경우,\n
+                            고객센터로 연락주시거나, 통화연결이 어려우신 경우 카카오톡 아이디(ID:kurly)로 문의해주세요\n
+                            성심껏 답변드리도록 하겠습니다.\n
+                            감사합니다.\n\n
+                            마켓컬리 일동.',
+                            '2016-01-12',
+                            '시스템 이전(1/12)에 따른 비밀번호 일괄 변경에 관한 공지',
+                            'MarketKurly'
+                        );
+insert into board (board_id, cnt, content, regdate, title, writer) values(
+                            null,
+                            2828,
+                            '4/5일 도입한 에코박스의 종료 시점관련하여 안내드립니다.\n\n\n
+                            - 종료 시점\n\n
+                            ~5/15 23:00까지(5/16 수령 샛별배송 주문건까지 에코박스 포장)\n\n
+                            * 상품 품질에 영향을 미칠 수 있을 정도로 급격한 온도변화가 있을 경우 종료 시점 이전에 포장법이 변경될 수 있습니다.\n\n\n
+                            종료 이후 새로운 에코박스 도입을 통해 고객님들꼐서 만족할 수 있도록 노력하는 컬리가 되도록 하겠습니다.\n\n
+                            맛있고 행복한 하루 되세요!\n\n',
+                            '2017-04-06',
+                            '에코박스 도입에 따른 한시적 포장 방법 변경 공지(종료 시점 확정)',
+                            'MarketKurly'
+                        );
+insert into board (board_id, cnt, content, regdate, title, writer) values(
+                            null,
+                            7190,
+                            '3월 23부터 주문 취소 가능시간이 오후 9시에서 오후 6시로 변경됩니다. 즉 전날 밤 11시부터 주문하신 모든 건은 오후 6시까지만 취소가 가능하며 오후 6시 이후에 주문해 주신 건의 경우 단순변심으로 인한 취소는 불가능합니다.\n
+                            마켓컬리는 밤 11시까지만 주문하시면 아침 7시까지 배송해드리는 국내에서 가장 빠른 배송을 선보이고 있는데요, 빠르고 정확한 배송을 위해 오후 6시부터는 입고된 상품의 포장이 시작되며 송장이 출력됩니다.\n
+                            송장 출력이 완료된 주문의 경우 취소 처리가 어려운 점 너그러운 양해 부탁 드립니다.\n\n
+                            언제나 더 좋은 서비스를 만들기 위해 노력 하겠습니다.\n\n
+                            감사합니다\n',
+                            '2016-03-22',
+                            '주문취소마감 시간 변경 공지',
+                            'MarketKurly'
+                        );
+insert into board (board_id, cnt, content, regdate, title, writer) values(
+                            null,
+                            2223,
+                            '현행 보냉수준 유짓 일부 상품 냉해 우려가 되고, 필요 이상의 포장재&보냉재를 사용하게 됨에 따라 동절기 동안 아래와 같이 변경하고자 하오니 이용에 참고 부탁드립니다.\n\n" +
+                            # 시행일자 11/28(화)수령건\n
+                            # 동절기 포장 적용 시 변경사함\n
+                             - 아이스젤 & 드라이아이스 수량 조정\n
+                             - 1차 포장 일부 상품 해제\n
+                               : 보냉갱화를 위해 은박파우치로 보냉포장 했던 육류 등 일부 상품은 1차 포장 해제 예정.\n
+                                 cf) 해산물 등 극신건 상품에 대해서는 1차 포장 유지\n\n\n\n
+                            좋은 품질과 서비스로 찾아뵙기 위해 항상 노력하겠습니다.\n
+                            추운 날씨 건강 유념하시고, 늘 행복하세요.\n\n
+                            감사합니다.\n
+                            마켓컬리 드림.',
+                            '2017-11-24',
+                            '[공지]동절기 포장 적용 안내(11/28 수령건~)',
+                            'MarketKurly'
+                        );
+
+-- goods
+insert into product (product_id, category, data, original_price, detail_img_url, detail_context, detail_title, product_img_url, name, brand_title, contactant, delivery_time_type_text, detail_image_url,
+                     discount_end_datetime, discount_percent, discounted_price, expiration_date, is_sales, list_image_url, main_image_url, origin, original_image_url, packing_type_text, short_description,
+                     sticker_image_url, unit_text, weight, guides)
+values (
+        3,
+        0,
+        '',
+        3300,
+        'https://img-cf.kurly.com/shop/data/goods/1609141191801m0.jpg',
+        '',
+        '',
+        '',
+        '친환경 당근 500g',
+        '',
+        '',
+        '0,1',
+        'https://img-cf.kurly.com/shop/data/goods/1609141191801m0.jpg',
+        '',
+        5,
+        3135,
+        '',
+        true,
+        'https://img-cf.kurly.com/shop/data/goods/1609141191486s0.jpg',
+        'https://img-cf.kurly.com/shop/data/goods/1609141191365i0.jpg',
+        '국내',
+        'https://img-cf.kurly.com/shop/data/goods/1609141186826l0.jpg',
+        '냉장/종이포장',
+        '껍질째 먹을 수 있는 친환경 흙당근 (500g 내외)',
+        'https://img-cf.kurly.com/shop/data/my_icon/icon_farming_coupon_20_percent.png',
+        '1봉지',
+        '500g(2~4개입)',
+        '["식품 특성상 중량은 5% 내외의 차이가 발생할 수 있습니다.", "해당 상품은 여러 협력업체에서 납품하고 있는 상품으로 수령 시, 상이한 협력업체의 상품을 받아보실 수 있습니다. 품질은 동일 기준으로 입고되고 있사오니 참고 부탁드립니다."]'
+);
+insert into product (product_id, category, data, original_price, detail_img_url, detail_context, detail_title, product_img_url, name, brand_title, contactant, delivery_time_type_text, detail_image_url,
+                     discount_end_datetime, discount_percent, discounted_price, expiration_date, is_sales, list_image_url, main_image_url, origin, original_image_url, packing_type_text, short_description,
+                     sticker_image_url, unit_text, weight, guides)
+values (
+           6,
+           0,
+           '',
+           3300,
+           'https://img-cf.kurly.com/shop/data/goods/1609141191801m0.jpg',
+           '',
+           '',
+           '',
+           '친환경 당근 500g',
+           '',
+           '',
+           '0,1',
+           'https://img-cf.kurly.com/shop/data/goods/1609141191801m0.jpg',
+           '',
+           5,
+           3135,
+           '',
+           true,
+           'https://img-cf.kurly.com/shop/data/goods/1609141191486s0.jpg',
+           'https://img-cf.kurly.com/shop/data/goods/1609141191365i0.jpg',
+           '국내',
+           'https://img-cf.kurly.com/shop/data/goods/1609141186826l0.jpg',
+           '냉장/종이포장',
+           '껍질째 먹을 수 있는 친환경 흙당근 (500g 내외)',
+           'https://img-cf.kurly.com/shop/data/my_icon/icon_farming_coupon_20_percent.png',
+           '1봉지',
+           '500g(2~4개입)',
+           '["식품 특성상 중량은 5% 내외의 차이가 발생할 수 있습니다.", "해당 상품은 여러 협력업체에서 납품하고 있는 상품으로 수령 시, 상이한 협력업체의 상품을 받아보실 수 있습니다. 품질은 동일 기준으로 입고되고 있사오니 참고 부탁드립니다."]'
+       );
+insert into product (product_id, category, data, original_price, detail_img_url, detail_context, detail_title, product_img_url, name, brand_title, contactant, delivery_time_type_text, detail_image_url,
+                     discount_end_datetime, discount_percent, discounted_price, expiration_date, is_sales, list_image_url, main_image_url, origin, original_image_url, packing_type_text, short_description,
+                     sticker_image_url, unit_text, weight, guides)
+values (
+           9,
+           0,
+           '',
+           3300,
+           'https://img-cf.kurly.com/shop/data/goods/1609141191801m0.jpg',
+           '',
+           '',
+           '',
+           '친환경 당근 500g',
+           '',
+           '',
+           '0,1',
+           'https://img-cf.kurly.com/shop/data/goods/1609141191801m0.jpg',
+           '',
+           5,
+           3135,
+           '',
+           true,
+           'https://img-cf.kurly.com/shop/data/goods/1609141191486s0.jpg',
+           'https://img-cf.kurly.com/shop/data/goods/1609141191365i0.jpg',
+           '국내',
+           'https://img-cf.kurly.com/shop/data/goods/1609141186826l0.jpg',
+           '냉장/종이포장',
+           '껍질째 먹을 수 있는 친환경 흙당근 (500g 내외)',
+           'https://img-cf.kurly.com/shop/data/my_icon/icon_farming_coupon_20_percent.png',
+           '1봉지',
+           '500g(2~4개입)',
+           '["식품 특성상 중량은 5% 내외의 차이가 발생할 수 있습니다.", "해당 상품은 여러 협력업체에서 납품하고 있는 상품으로 수령 시, 상이한 협력업체의 상품을 받아보실 수 있습니다. 품질은 동일 기준으로 입고되고 있사오니 참고 부탁드립니다."]'
+       );
+insert into product (product_id, category, data, original_price, detail_img_url, detail_context, detail_title, product_img_url, name, brand_title, contactant, delivery_time_type_text, detail_image_url,
+                     discount_end_datetime, discount_percent, discounted_price, expiration_date, is_sales, list_image_url, main_image_url, origin, original_image_url, packing_type_text, short_description,
+                     sticker_image_url, unit_text, weight, guides)
+values (
+           12,
+           0,
+           '',
+           3300,
+           'https://img-cf.kurly.com/shop/data/goods/1609141191801m0.jpg',
+           '',
+           '',
+           '',
+           '친환경 당근 500g',
+           '',
+           '',
+           '0,1',
+           'https://img-cf.kurly.com/shop/data/goods/1609141191801m0.jpg',
+           '',
+           5,
+           3135,
+           '',
+           true,
+           'https://img-cf.kurly.com/shop/data/goods/1609141191486s0.jpg',
+           'https://img-cf.kurly.com/shop/data/goods/1609141191365i0.jpg',
+           '국내',
+           'https://img-cf.kurly.com/shop/data/goods/1609141186826l0.jpg',
+           '냉장/종이포장',
+           '껍질째 먹을 수 있는 친환경 흙당근 (500g 내외)',
+           'https://img-cf.kurly.com/shop/data/my_icon/icon_farming_coupon_20_percent.png',
+           '1봉지',
+           '500g(2~4개입)',
+           '["식품 특성상 중량은 5% 내외의 차이가 발생할 수 있습니다.", "해당 상품은 여러 협력업체에서 납품하고 있는 상품으로 수령 시, 상이한 협력업체의 상품을 받아보실 수 있습니다. 품질은 동일 기준으로 입고되고 있사오니 참고 부탁드립니다."]'
+       );
+insert into product (product_id, category, data, original_price, detail_img_url, detail_context, detail_title, product_img_url, name, brand_title, contactant, delivery_time_type_text, detail_image_url,
+                     discount_end_datetime, discount_percent, discounted_price, expiration_date, is_sales, list_image_url, main_image_url, origin, original_image_url, packing_type_text, short_description,
+                     sticker_image_url, unit_text, weight, guides)
+values (
+           15,
+           0,
+           '',
+           3300,
+           'https://img-cf.kurly.com/shop/data/goods/1609141191801m0.jpg',
+           '',
+           '',
+           '',
+           '친환경 당근 500g',
+           '',
+           '',
+           '0,1',
+           'https://img-cf.kurly.com/shop/data/goods/1609141191801m0.jpg',
+           '',
+           5,
+           3135,
+           '',
+           true,
+           'https://img-cf.kurly.com/shop/data/goods/1609141191486s0.jpg',
+           'https://img-cf.kurly.com/shop/data/goods/1609141191365i0.jpg',
+           '국내',
+           'https://img-cf.kurly.com/shop/data/goods/1609141186826l0.jpg',
+           '냉장/종이포장',
+           '껍질째 먹을 수 있는 친환경 흙당근 (500g 내외)',
+           'https://img-cf.kurly.com/shop/data/my_icon/icon_farming_coupon_20_percent.png',
+           '1봉지',
+           '500g(2~4개입)',
+           '["식품 특성상 중량은 5% 내외의 차이가 발생할 수 있습니다.", "해당 상품은 여러 협력업체에서 납품하고 있는 상품으로 수령 시, 상이한 협력업체의 상품을 받아보실 수 있습니다. 품질은 동일 기준으로 입고되고 있사오니 참고 부탁드립니다."]'
+       );
+insert into product (product_id, category, data, original_price, detail_img_url, detail_context, detail_title, product_img_url, name, brand_title, contactant, delivery_time_type_text, detail_image_url,
+                     discount_end_datetime, discount_percent, discounted_price, expiration_date, is_sales, list_image_url, main_image_url, origin, original_image_url, packing_type_text, short_description,
+                     sticker_image_url, unit_text, weight, guides)
+values (
+           18,
+           0,
+           '',
+           3300,
+           'https://img-cf.kurly.com/shop/data/goods/1609141191801m0.jpg',
+           '',
+           '',
+           '',
+           '친환경 당근 500g',
+           '',
+           '',
+           '0,1',
+           'https://img-cf.kurly.com/shop/data/goods/1609141191801m0.jpg',
+           '',
+           5,
+           3135,
+           '',
+           true,
+           'https://img-cf.kurly.com/shop/data/goods/1609141191486s0.jpg',
+           'https://img-cf.kurly.com/shop/data/goods/1609141191365i0.jpg',
+           '국내',
+           'https://img-cf.kurly.com/shop/data/goods/1609141186826l0.jpg',
+           '냉장/종이포장',
+           '껍질째 먹을 수 있는 친환경 흙당근 (500g 내외)',
+           'https://img-cf.kurly.com/shop/data/my_icon/icon_farming_coupon_20_percent.png',
+           '1봉지',
+           '500g(2~4개입)',
+           '["식품 특성상 중량은 5% 내외의 차이가 발생할 수 있습니다.", "해당 상품은 여러 협력업체에서 납품하고 있는 상품으로 수령 시, 상이한 협력업체의 상품을 받아보실 수 있습니다. 품질은 동일 기준으로 입고되고 있사오니 참고 부탁드립니다."]'
+       );
+insert into product (product_id, category, data, original_price, detail_img_url, detail_context, detail_title, product_img_url, name, brand_title, contactant, delivery_time_type_text, detail_image_url,
+                     discount_end_datetime, discount_percent, discounted_price, expiration_date, is_sales, list_image_url, main_image_url, origin, original_image_url, packing_type_text, short_description,
+                     sticker_image_url, unit_text, weight, guides)
+values (
+           21,
+           0,
+           '',
+           3300,
+           'https://img-cf.kurly.com/shop/data/goods/1609141191801m0.jpg',
+           '',
+           '',
+           '',
+           '친환경 당근 500g',
+           '',
+           '',
+           '0,1',
+           'https://img-cf.kurly.com/shop/data/goods/1609141191801m0.jpg',
+           '',
+           5,
+           3135,
+           '',
+           true,
+           'https://img-cf.kurly.com/shop/data/goods/1609141191486s0.jpg',
+           'https://img-cf.kurly.com/shop/data/goods/1609141191365i0.jpg',
+           '국내',
+           'https://img-cf.kurly.com/shop/data/goods/1609141186826l0.jpg',
+           '냉장/종이포장',
+           '껍질째 먹을 수 있는 친환경 흙당근 (500g 내외)',
+           'https://img-cf.kurly.com/shop/data/my_icon/icon_farming_coupon_20_percent.png',
+           '1봉지',
+           '500g(2~4개입)',
+           '["식품 특성상 중량은 5% 내외의 차이가 발생할 수 있습니다.", "해당 상품은 여러 협력업체에서 납품하고 있는 상품으로 수령 시, 상이한 협력업체의 상품을 받아보실 수 있습니다. 품질은 동일 기준으로 입고되고 있사오니 참고 부탁드립니다."]'
+       );

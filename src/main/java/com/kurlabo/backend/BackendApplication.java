5637c38b846261cc33b14ab358bada6afa8c2fb3
LNoah <limnoah0301@gmail.com>
2021-07-22 16:11:54 +0900

    refactor: Update BackendApplication.java
    Delete setting configuration

--- src/main/java/com/kurlabo/backend/BackendApplication.java
+++ src/main/java/com/kurlabo/backend/BackendApplication.java
@@ -5,13 +5,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class BackendApplication {
-
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.yml";
-
     public static void main(String[] args) {
         new SpringApplicationBuilder(BackendApplication.class)
-                .properties(APPLICATION_LOCATIONS)
                 .run(args);
     }
 }

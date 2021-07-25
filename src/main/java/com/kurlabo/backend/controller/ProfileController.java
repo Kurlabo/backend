213cd16014e85da8ed177a19f2f48b95c509a006
LNoah <limnoah0301@gmail.com>
2021-07-25 23:33:52 +0900

    refactor: Update ProfileController.java
    Delete annotation memo

--- src/main/java/com/kurlabo/backend/controller/ProfileController.java
+++ src/main/java/com/kurlabo/backend/controller/ProfileController.java
@@ -15,9 +15,6 @@ public class ProfileController {
 
     @GetMapping("/profile")
     public String profile(){
-        // 현재 실행 중인 ActiveProfile을 모두 가져온다.
-        // 즉, real, oauth, real-db 등이 활성화 되어 있다면 (active) 3개가 모두 담겨 있다.
-        // 여기서 real, real1, real2는 모두 배포에 사용될 profile이라 이 중 하나라도 있으면 그 값을 반환하도록 한다.
         List<String> profiles = Arrays.asList(environment.getActiveProfiles());
         List<String> realProfiles = Arrays.asList("real", "real1", "real2");
         String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);

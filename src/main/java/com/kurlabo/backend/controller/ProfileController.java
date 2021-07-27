97d55c538abfb52e0f70969edea6643aa6be98e1
LNoah <limnoah0301@gmail.com>
2021-07-27 15:15:00 +0900

    refactor: Update ProfileController.java
    Modified profile api's return value.

--- src/main/java/com/kurlabo/backend/controller/ProfileController.java
+++ src/main/java/com/kurlabo/backend/controller/ProfileController.java
@@ -5,6 +5,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,6 +20,6 @@ public class ProfileController {
         List<String> realProfiles = Arrays.asList("real", "real1", "real2");
         String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
 
-        return profiles.stream().filter(realProfiles::contains).findAny().orElse(defaultProfile);
+        return "profile : " + profiles.stream().filter(realProfiles::contains).findAny().orElse(defaultProfile) + ", Modified Time : " + LocalDateTime.now();
     }
 }

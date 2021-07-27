7ed583034f9c908a7e14ad5c0a3fdc4f8e497a95
LNoah <limnoah0301@gmail.com>
2021-07-27 16:39:07 +0900

    conf: Update .gitignore

--- src/main/java/com/kurlabo/backend/repository/db/InsertDBRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kurlabo.backend.repository.db;
-
-import com.kurlabo.backend.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InsertDBRepository extends JpaRepository<Product, Long> {
-}

7ae3b33d64a2faacd5844292fc228d7c7d449c76
LNoah <limnoah0301@gmail.com>
2021-08-03 01:31:51 +0900

    refactor: Update GlobalExceptionHandler.java
    Modified methods return value.

--- src/main/java/com/kurlabo/backend/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/kurlabo/backend/exception/handler/GlobalExceptionHandler.java
@@ -1,7 +1,8 @@
 package com.kurlabo.backend.exception.handler;
 
-import com.kurlabo.backend.dto.exception.ErrorDto;
+import com.kurlabo.backend.dto.exception.ExceptionDto;
 import com.kurlabo.backend.exception.DataNotFoundException;
+import com.kurlabo.backend.exception.InvalidTokenException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -10,8 +11,19 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-//    @ExceptionHandler(DataNotFoundException.class)
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    public ErrorDto
+    @ExceptionHandler(DataNotFoundException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionDto DataNotFoundExceptionHandler(DataNotFoundException e) {
+        return ExceptionDto.builder()
+                .message(e.getMessage())
+                .build();
+    }
 
+    @ExceptionHandler(InvalidTokenException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionDto InvalidTokenExceptionHandler(InvalidTokenException e) {
+        return ExceptionDto.builder()
+                .message(e.getMessage())
+                .build();
+    }
 }

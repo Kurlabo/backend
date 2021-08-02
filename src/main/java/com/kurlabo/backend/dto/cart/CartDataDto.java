fadc48e17c398ed8b8f11fdfc077ee3a68dae125
LNoah <limnoah0301@gmail.com>
2021-08-03 02:27:39 +0900

    refactor: Update CartDataDto.java
    Add annotations
    - AllArgsConstructor
    - NoArgsConstructor
    - Data

--- src/main/java/com/kurlabo/backend/dto/cart/CartDataDto.java
+++ src/main/java/com/kurlabo/backend/dto/cart/CartDataDto.java
@@ -1,7 +1,13 @@
 package com.kurlabo.backend.dto.cart;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class CartDataDto {
     private Long product_id;

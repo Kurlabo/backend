aee4526ce36e0167d8067027e1b8c79221d6dbf1
LNoah <limnoah0301@gmail.com>
2021-08-02 15:12:48 +0900

    docs: Update Repositories import package
    Modified Insta_src, Main_src, Slide_img 's package.

--- src/main/java/com/kurlabo/backend/repository/MainSrcRepository.java
+++ src/main/java/com/kurlabo/backend/repository/MainSrcRepository.java
@@ -1,10 +1,8 @@
 package com.kurlabo.backend.repository;
 
-import com.kurlabo.backend.model.db.Main_src;
+import com.kurlabo.backend.model.Main_src;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface MainSrcRepository extends JpaRepository<Main_src, Long> {
     @Query("select m.setbyul_img from Main_src m where m.id = 1")

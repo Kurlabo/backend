a962e1299437e81e8c636f596cff28e2f251f32e
LNoah <limnoah0301@gmail.com>
2021-08-03 01:08:56 +0900

    refactor: Remove ResourceNotFoundException import

--- src/main/java/com/kurlabo/backend/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/kurlabo/backend/exception/handler/GlobalExceptionHandler.java
@@ -1,7 +1,7 @@
 package com.kurlabo.backend.exception.handler;
 
 import com.kurlabo.backend.dto.exception.ErrorDto;
-import com.kurlabo.backend.exception.ResourceNotFoundException;
+import com.kurlabo.backend.exception.DataNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-//    @ExceptionHandler(ResourceNotFoundException.class)
+//    @ExceptionHandler(DataNotFoundException.class)
 //    @ResponseStatus(HttpStatus.BAD_REQUEST)
 //    public ErrorDto
 

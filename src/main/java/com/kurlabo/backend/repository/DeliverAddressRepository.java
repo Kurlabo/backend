81a328a284223fa167d78f1d018732aa09052b7f
LNoah <limnoah0301@gmail.com>
2021-08-05 03:07:38 +0900

    refactor: Update DeliverAddressRepository.java
    Add Query annotation to findByMemberAndIs_main method

--- src/main/java/com/kurlabo/backend/repository/DeliverAddressRepository.java
+++ src/main/java/com/kurlabo/backend/repository/DeliverAddressRepository.java
@@ -3,6 +3,7 @@ package com.kurlabo.backend.repository;
 import com.kurlabo.backend.model.Deliver_Address;
 import com.kurlabo.backend.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,6 +12,7 @@ public interface DeliverAddressRepository extends JpaRepository<Deliver_Address,
 
     List<Deliver_Address> findByMember(Member member);
 
+    @Query("select da from Deliver_Address da where da.member = :member and da.is_main = :isMain")
     Optional<Deliver_Address> findByMemberAndIs_main(Member member, int isMain);
 
 }

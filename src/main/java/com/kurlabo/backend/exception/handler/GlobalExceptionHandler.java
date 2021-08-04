a0fd809c5f55467897431d06fc02130638424e8f
LNoah <limnoah0301@gmail.com>
2021-08-04 15:57:40 +0900

    refactor: Update GlobalExceptionHandlet.java
    Add method for InvalidPasswordException

--- src/main/java/com/kurlabo/backend/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/kurlabo/backend/exception/handler/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.kurlabo.backend.exception.handler;
 import com.kurlabo.backend.dto.exception.ExceptionDto;
 import com.kurlabo.backend.exception.DataNotFoundException;
 import com.kurlabo.backend.exception.InvalidCartCntException;
+import com.kurlabo.backend.exception.InvalidPasswordException;
 import com.kurlabo.backend.exception.InvalidTokenException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -31,6 +32,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(InvalidCartCntException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ExceptionDto InvalidCartCntExceptionHandler(InvalidCartCntException e) {
+        return ExceptionDto.builder()
+                .message(e.getMessage())
+                .build();
+    }
+
+    @ExceptionHandler(InvalidPasswordException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionDto InvalidPasswordException(InvalidCartCntException e) {
         return ExceptionDto.builder()
                 .message(e.getMessage())
                 .build();

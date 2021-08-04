f52e3e993132fa8da84feea2c5fe50bf04c4b9f7
LNoah <limnoah0301@gmail.com>
2021-08-05 02:50:26 +0900

    refactor: Update CheckoutProductsDto.java
    Create constructor

--- src/main/java/com/kurlabo/backend/dto/cart/CheckoutProductsDto.java
+++ src/main/java/com/kurlabo/backend/dto/cart/CheckoutProductsDto.java
@@ -1,5 +1,6 @@
 package com.kurlabo.backend.dto.cart;
 
+import com.kurlabo.backend.model.Order_Sheet_Products;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,8 +12,18 @@ public class CheckoutProductsDto {
     private Long product_id;
     private String product_name;
     private int product_price;
-    private int product_discount_price;
+    private int discount_price;
     private int product_cnt;
     private String list_image_url;
     private int review_status;
+
+    public CheckoutProductsDto(Order_Sheet_Products orderSheet) {
+        this.product_id = orderSheet.getProduct_id();
+        this.product_name = orderSheet.getProduct_name();
+        this.product_price = orderSheet.getProduct_price();
+        this.discount_price = orderSheet.getDiscounted_price();
+        this.product_cnt = orderSheet.getProduct_cnt();
+        this.list_image_url = orderSheet.getList_image_url();
+        this.review_status = orderSheet.getReview_status();
+    }
 }

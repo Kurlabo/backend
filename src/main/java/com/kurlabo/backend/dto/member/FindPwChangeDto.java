0f69d4d6c01163081ba4300040e63ebda403e55f
LNoah <limnoah0301@gmail.com>
2021-08-05 01:24:02 +0900

    refactor: Update FindPwChangeDto.java
    Remove unused import.

--- src/main/java/com/kurlabo/backend/dto/member/FindPwChangeDto.java
+++ src/main/java/com/kurlabo/backend/dto/member/FindPwChangeDto.java
@@ -5,9 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor

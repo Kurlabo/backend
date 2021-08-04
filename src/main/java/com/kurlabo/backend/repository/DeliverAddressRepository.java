6b68c8939f8c3c729efb2b4a4f4d3225fe55c9a6
LNoah <limnoah0301@gmail.com>
2021-08-05 01:22:41 +0900

    refactor: Update DeliverAddressRepository.java
    Create method
    - findByMemberAndIs_main

--- src/main/java/com/kurlabo/backend/repository/DeliverAddressRepository.java
+++ src/main/java/com/kurlabo/backend/repository/DeliverAddressRepository.java
@@ -5,7 +5,11 @@ import com.kurlabo.backend.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DeliverAddressRepository extends JpaRepository<Deliver_Address, Long> {
+    
     List<Deliver_Address> findByMember(Member member);
+
+    Optional<Deliver_Address> findByMemberAndIs_main(Member member, int isMain);
 }

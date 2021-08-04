de73e108c1913f2ac25570ac7aa45fc141422e88
LNoah <limnoah0301@gmail.com>
2021-08-04 19:36:18 +0900

    refactor: Update Cart.java
    Create method
    - toCartProductDto

--- src/main/java/com/kurlabo/backend/model/Cart.java
+++ src/main/java/com/kurlabo/backend/model/Cart.java
@@ -1,5 +1,7 @@
 package com.kurlabo.backend.model;
 
+import com.kurlabo.backend.dto.cart.CartProductDto;
+import com.kurlabo.backend.dto.cart.GetCartResponseDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,4 +24,19 @@ public class Cart {
     @ManyToOne
     @JoinColumn(name = "member_id")
     private Member member;
+
+    public CartProductDto toCartProductDto(Product product) {
+        return CartProductDto.builder()
+                .product_id(product.getId())
+                .name(product.getName())
+                .original_price(product.getOriginal_price())
+                .discounted_price(product.getDiscounted_price())
+                .packing_type_text(product.getPacking_type_text())
+                .min_ea(1)
+                .max_ea(99)
+                .list_image_url(product.getList_image_url())
+                .cnt(this.getCnt())
+                .reduced_price(product.getOriginal_price()-product.getDiscounted_price())
+                .build();
+    }
 }

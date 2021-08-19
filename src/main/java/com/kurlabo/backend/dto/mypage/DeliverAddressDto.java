d5acbe602d14c8ce7e0f53d26cd459754c4ba019
LNoah <limnoah0301@gmail.com>
2021-08-20 01:45:11 +0900

    feat: Create DeliverAddressDto.java

---
+++ src/main/java/com/kurlabo/backend/dto/mypage/DeliverAddressDto.java
@@ -0,0 +1,31 @@
+package com.kurlabo.backend.dto.mypage;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class DeliverAddressDto {
+    private Long id;
+    private String deliver_address;
+    private String deliver_detail_address;
+    private int is_main;
+    private String reciever;
+    private String reciever_phone;
+    private int checked;
+
+    @QueryProjection
+    public DeliverAddressDto(Long id, String deliver_address, String deliver_detail_address, int is_main, String reciever, String reciever_phone){
+        this.id = id;
+        this.deliver_address = deliver_address;
+        this.deliver_detail_address = deliver_detail_address;
+        this.is_main = is_main;
+        this.reciever = reciever;
+        this.reciever_phone = reciever_phone;
+    }
+}

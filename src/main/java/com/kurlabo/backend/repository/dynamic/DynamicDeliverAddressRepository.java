0265e0e28af063b718554d27d35a5a1343c8cabe
LNoah <limnoah0301@gmail.com>
2021-08-23 01:17:40 +0900

    refactor: Update DynamicDeliverAddressRepository.java

--- src/main/java/com/kurlabo/backend/repository/dynamic/DynamicDeliverAddressRepository.java
+++ src/main/java/com/kurlabo/backend/repository/dynamic/DynamicDeliverAddressRepository.java
@@ -1,6 +1,8 @@
 package com.kurlabo.backend.repository.dynamic;
 
 import com.kurlabo.backend.dto.mypage.DeliverAddressDto;
+import com.kurlabo.backend.dto.mypage.QDeliverAddressDto;
+import com.kurlabo.backend.model.Member;
 import com.kurlabo.backend.model.QDeliver_Address;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -11,9 +13,17 @@ import java.util.List;
 @Repository
 @RequiredArgsConstructor
 public class DynamicDeliverAddressRepository {
+
     private final JPAQueryFactory queryFactory;
 
     QDeliver_Address qDeliverAddress = QDeliver_Address.deliver_Address;
 
-//    public List<DeliverAddressDto> findByMember
+    public List<DeliverAddressDto> findByMember(Member member){
+        return queryFactory
+                .select(new QDeliverAddressDto(qDeliverAddress.id, qDeliverAddress.deliver_address, qDeliverAddress.deliver_detail_address,
+                        qDeliverAddress.is_main, qDeliverAddress.reciever, qDeliverAddress.reciever_phone))
+                .from(qDeliverAddress)
+                .where(qDeliverAddress.member.eq(member))
+                .fetch();
+    }
 }

5b99414a834be53636c7083b479b0ac2b30417eb
LNoah <limnoah0301@gmail.com>
2021-08-25 01:01:28 +0900

    refactor: Update ReviewRepository.java
    Remove useless code and annotations

--- src/main/java/com/kurlabo/backend/repository/ReviewRepository.java
+++ src/main/java/com/kurlabo/backend/repository/ReviewRepository.java
@@ -6,24 +6,10 @@ import com.kurlabo.backend.model.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long > {
-
-//    @Transactional
-//    @Modifying
-//    @Query("update Review r set r.help = r.help + 1 where r.review_id = :review_id")
-//    int updateHelpCnt(@Param("review_id") Long review_id);
-//    // int/Integer 반환만 가능
-//    // Modifying는 좋은 방법이 아님 -> 대신 select 하기 전에 데이터베이스에 반영을 해주는 게 좋음
-
-//    @Transactional
-//    @Modifying
-//    @Query("update Review r set r.cnt = r.cnt + 1 where r.review_id = :review_id")
-//    int updateViewCnt(@Param("review_id") Long review_id);
 
     Page<Review> findAllByProduct(Product product, Pageable pageable);
 

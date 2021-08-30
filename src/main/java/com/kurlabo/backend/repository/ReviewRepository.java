073330b7d0b83dcf71dcb5dc7aac518a4a927281
LNoah <limnoah0301@gmail.com>
2021-08-31 00:18:27 +0900

    refactor: Update ReviewRepository.java

--- src/main/java/com/kurlabo/backend/repository/ReviewRepository.java
+++ src/main/java/com/kurlabo/backend/repository/ReviewRepository.java
@@ -1,18 +1,13 @@
 package com.kurlabo.backend.repository;
 
-import com.kurlabo.backend.model.Member;
 import com.kurlabo.backend.model.Product;
 import com.kurlabo.backend.model.Review;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long > {
 
-    Page<Review> findAllByProduct(Product product, Pageable pageable);
+    List<Review> findAllByProduct(Product product);
 
-    // @Query("select r from Review r where r.member = :member and r.product = :product")
-    List<Review> findByMemberAndProduct(Member member, Product product);
 }
